6f8a318c117130c40d8470c9eb4c6bd88b1d191d
David Pedan <david.ppdn@gmail.com>
2026-09-15 18:39:17 +1000

    Create initial repository structure: - Client Console App - Server Console App - Shared Class Library
    Created initial version of TCP server.

---
+++ src/NetBox.Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+public static class Program
+{
+    private static readonly IPAddress IpAddress = IPAddress.Loopback;
+    private static readonly int Port = 5000;
+
+    public static async Task Main(string[] args)
+    {
+        var ipEndPoint = new IPEndPoint(IpAddress, Port);
+        TcpListener listener = new TcpListener(ipEndPoint);
+
+        try
+        {
+            listener.Start();
+
+            Console.WriteLine($"Listening on {IpAddress}:{Port}");
+
+            // Waits asynchronously until a client establishes a TCP connection
+            using TcpClient handler = await listener.AcceptTcpClientAsync();
+            await using NetworkStream stream = handler.GetStream();
+
+            Console.WriteLine("Client connected.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+}
---
+++ src/NetBox.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace NetBox.Shared;
+
+public class Class1
+{
+
+}
