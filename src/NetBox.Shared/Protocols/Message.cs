4bcbaf3894327adb61e6090483c3d4b6b911c152
David Pedan <david.ppdn@gmail.com>
2026-09-19 19:57:40 +1000

    Added intial constructor for message based on defined protocol, removed stale message tests.

--- src/NetBox.Shared/Protocols/Message.cs
+++ src/NetBox.Shared/Protocols/Message.cs
@@ -4,8 +4,23 @@ using System.Text;
 
 namespace NetBox.Shared.Protocols;
 
-public record Message(string Content)
+public class Message
 {
-    public byte[] ToBytes() 
-        => Encoding.UTF8.GetBytes(Content + "\n");
+    private Header _header { get; } = new Header();
+    private byte[] _payload { get; } = Array.Empty<byte>();
+
+    /// <summary>
+    /// Constructor with Header and payload.
+    /// Payload for ease of use should be just strin, which gets converted to UT8.
+    /// </summary>
+    /// <param name="header"></param>
+    /// <param name="payload"></param>
+    public Message(Header header, string payload)
+    {
+        ArgumentNullException.ThrowIfNull(header);
+        ArgumentNullException.ThrowIfNull(payload);
+
+        _header = header;
+        _payload = Encoding.UTF8.GetBytes(payload);
+    }
 }
