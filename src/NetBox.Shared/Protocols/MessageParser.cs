e1c591c6f674872f0e1898bf551522eeaeac59a7
David Pedan <david.ppdn@gmail.com>
2026-09-20 13:03:22 +1000

    Finished intial iteration of MessageParser for new protocol. Will need test cases.

---
+++ src/NetBox.Shared/Protocols/MessageParser.cs
@@ -0,0 +1,57 @@
+﻿using System.Buffers.Binary;
+
+namespace NetBox.Shared.Protocols;
+
+public class MessageParser
+{
+    private readonly List<byte> _buffer = [];
+
+    public MessageParser()
+    {
+    }
+
+    public List<Message> ParseBytes(byte[] data)
+    {
+        var messages = new List<Message>();
+        _buffer.AddRange(data);
+
+        while (true)
+        {
+            var buffer = _buffer.ToArray();
+
+            if (_buffer.Count < 4)
+            {
+                // Not enough data to read message length
+                break;
+            }
+
+            var messageLength = BinaryPrimitives.ReadInt32BigEndian(buffer.AsSpan());
+            if (messageLength < 0)
+            {
+                throw new InvalidDataException("Invalid message length.");
+            }
+
+            if (_buffer.Count - 4 < messageLength)
+            {
+                // Not enough data for one complete message
+                break;
+            }
+
+            var messageBytes = buffer.AsSpan(4, messageLength);
+            if (!Message.Deserialize(messageBytes.ToArray(), out var message))
+            {
+                throw new InvalidDataException("Failed to deserialize a message");
+            }
+
+            if (message == null)
+            {
+                throw new InvalidDataException("Failed to return a message");
+            }
+
+            messages.Add(message);
+            _buffer.RemoveRange(0, 4 + messageLength);
+        }
+
+        return messages;
+    }
+}
